541c61e8554f492e73869047ae177a9acc2b68bd
DoubleHong <greathongtian@gmail.com>
2023-01-16 17:04:54 +0800

    Hong

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import AdministratorView from '../OrganizationAdministratorViews/AdministratorView.vue'
+import AdministratorView from '../views/OrganizationAdministratorViews/AdministratorView.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
